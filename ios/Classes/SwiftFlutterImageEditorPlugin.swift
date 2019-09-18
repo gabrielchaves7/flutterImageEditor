5ded9bd713c2f899e49673cbf75dd323f5249f8e
pedro <estanislau.pedro@gmail.com>
2019-09-18 15:41:04 -0300

    edit brightness and contrast for ios

--- ios/Classes/SwiftFlutterImageEditorPlugin.swift
+++ ios/Classes/SwiftFlutterImageEditorPlugin.swift
@@ -13,8 +13,7 @@ public class SwiftFlutterImageEditorPlugin: NSObject, FlutterPlugin {
         let argsMap = call.arguments as! NSDictionary
         let foto:FlutterStandardTypedData = argsMap.value(forKey: "foto") as! FlutterStandardTypedData
         let brilho:Double = argsMap.value(forKey: "brilho") as! Double
-        let contraste:Double = argsMap.value(forKey: "contraste") as! Double
-        
+        let contraste:Double = argsMap.value(forKey: "contraste") as! Double        
         let image = UIImage(data: foto.data)!
 
         result(self.changeBitmapContrastBrightness(imagem: image, brightness: brilho, contrast: contraste))
@@ -26,7 +25,7 @@ public class SwiftFlutterImageEditorPlugin: NSObject, FlutterPlugin {
     }    
   }
 
-  private func changeBitmapContrastBrightness(imagem: UIImage, brightness: Double, contrast: Double) -> UIImage{
+  private func changeBitmapContrastBrightness(imagem: UIImage, brightness: Double, contrast: Double) -> FlutterStandardTypedData{
     let inputImage = CIImage(image: imagem)!
     let parameters = [
         "inputBrightness": NSNumber(value: brightness),
@@ -36,7 +35,9 @@ public class SwiftFlutterImageEditorPlugin: NSObject, FlutterPlugin {
 
     let context = CIContext(options: nil)
     let img = context.createCGImage(outputImage, from: outputImage.extent)!
-    print("deu erro ao editar a imagem")
-    return UIImage(cgImage: img)
+    let uimg = UIImage(cgImage: img)
+    
+    return FlutterStandardTypedData.init(bytes: uimg.jpegData(compressionQuality: 1)!)
+
   }
 }
